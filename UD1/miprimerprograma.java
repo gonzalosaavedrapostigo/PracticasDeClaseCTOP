1c9c34bbe9fbe3ce15b2e1f278c51d428e15458f
gonzalosaavedrapostigo <saavedrapostigo.gonzalo@gmail.com>
2025-09-30 10:21:42 +0200

    cambios en miprimerprograma

--- UD1/miprimerprograma.java
+++ UD1/miprimerprograma.java
@@ -3,5 +3,11 @@ package UD1;
 public class miprimerprograma {
     public static void main(String[] args) {
         System.out.println("Hola Adrián y Daniel");
+        System.out.println("Hola Adrián y Daniel");
+        System.out.println("Hola Adrián y Daniel");
+        System.out.println("Hola Adrián y Daniel");
+        System.out.println("Hola Adrián y Daniel");
+        System.out.println("Hola Adrián y Daniel");
+        System.out.println("Hola Adrián y Daniel");
     }
 }

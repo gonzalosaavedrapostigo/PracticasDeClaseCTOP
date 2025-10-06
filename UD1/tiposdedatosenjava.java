b73a5bd244abb9cab5e98a6e2a085f403a7b7ed4
gonzalosaavedrapostigo <saavedrapostigo.gonzalo@gmail.com>
2025-10-06 09:31:45 +0200

    nuevo fichero tipos de datos

---
+++ UD1/tiposdedatosenjava.java
@@ -0,0 +1,36 @@
+package UD1;
+
+public class tiposdedatosenjava {
+    public static void main(String[] args) {
+        // Voy a poner mi código aquí
+
+        /* Las variables son nombre a los cuales podemos asignar datos
+         * Tenemos que definir el tipo de dato que va a tener la variable
+         * El nombre deb dejar claro cual es el dato que va a guardar
+         */
+
+         // Tipo de datos númericos
+
+         // a) tipo de dato entero
+         int edad;
+         int edadInicial;
+         int edadPadre;
+
+         edad = 28;
+
+         // Variables de tipo decimal
+         double pi = 3.1416;
+         float peso = 90.5f;
+         
+        // Tipo para cadenas de texto y caracteres
+        String nombre = "Héctor";
+        char caracter = 'b';
+
+        // Tipo de dato booleano
+        Boolean TengoCarnetDeConducir = true;
+
+
+        // Aqui se escribe el código del programa
+        System.out.println("Hector Cordero tiene " + edad + " años");
+    }
+}

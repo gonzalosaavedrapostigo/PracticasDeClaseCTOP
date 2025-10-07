bb844d806e3cef0eae402ca2294ea95a32235c7b
gonzalosaavedrapostigo <saavedrapostigo.gonzalo@gmail.com>
2025-10-07 10:27:57 +0200

    cuidado alvaro que he tocado estos ficheros

---
+++ UD1/Variables.java
@@ -0,0 +1,12 @@
+package UD1;
+
+public class Variables {
+    public static void main(String[] args) {
+        float numero = 24.4f;
+
+        // Da error
+        float pi = 3.1416f;
+
+        //double pi = 3;
+    }
+}
--- UD1/tiposdedatosenjava.java
+++ UD1/tiposdedatosenjava.java
@@ -11,23 +11,33 @@ public class tiposdedatosenjava {
 
          // Tipo de datos númericos
 
-         // a) tipo de dato entero
+         // a) variables de tipo de dato entero
          int edad;
          int edadInicial;
          int edadPadre;
 
          edad = 28;
+         edad = 30;
+         edad = 35;
 
          // Variables de tipo decimal
          double pi = 3.1416;
-         float peso = 90.5f;
+         float peso = 90.578678968f;
+
+         // En una sola linea puedemos definir varias variables, separadas por comas
+         //double altura, altura2;
+
+         double altura = 1.75, altura2 = 1.80;
+
          
         // Tipo para cadenas de texto y caracteres
-        String nombre = "Héctor";
-        char caracter = 'b';
+        String nombre = "Héctor18";
+        char caracter = 'm';
 
         // Tipo de dato booleano
+        // Este tipo de dato solo puede tener dos valores: true o false
         Boolean TengoCarnetDeConducir = true;
+        TengoCarnetDeConducir = false;
 
 
         // Aqui se escribe el código del programa
